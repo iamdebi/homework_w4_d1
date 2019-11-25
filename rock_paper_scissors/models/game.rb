f8193f1c712d3960af94ae5823037bd71c3bb3d4
Debbie <debiurquhart@live.co.uk>
2019-11-25 15:32:35 +0000

    adds game logic to game.rb

--- rock_paper_scissors/models/game.rb
+++ rock_paper_scissors/models/game.rb
@@ -1,1 +1,29 @@
+class Game
 
+  def self.winner(item1, item2)
+    case item1
+    when "rock"
+      case item2
+      when  "paper"
+        return "paper wins"
+      when "scissors"
+        return "rock wins"
+      end
+    when "scissors"
+      case item2
+      when "paper"
+        return "scissors wins"
+      when "rock"
+        return "rock wins"
+      end
+    when "paper"
+      case item2
+      when "rock"
+        return "paper wins"
+      when "scissors"
+        return "scissors wins"
+      end
+    end
+  end
+
+end
