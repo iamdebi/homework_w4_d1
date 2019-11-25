a69a70832eae05f3cd063a5d5901e0b2a690d532
Debbie <debiurquhart@live.co.uk>
2019-11-25 15:31:57 +0000

    adds test functions for all types of results

---
+++ rock_paper_scissors/specs/game_spec.rb
@@ -0,0 +1,33 @@
+require('minitest/autorun')
+require('minitest/reporters')
+require_relative('../models/game')
+
+Minitest:: Reporters.use! Minitest::Reporters::SpecReporter.new
+
+class GameTest<MiniTest::Test
+
+  def test_rock_beats_scissors_1
+    assert_equal("rock wins", Game.winner("rock", "scissors"))
+  end
+
+  def test_rock_beats_scissors_2
+    assert_equal("rock wins", Game.winner("scissors", "rock"))
+  end
+
+  def test_paper_beats_rock_1
+    assert_equal("paper wins", Game.winner("rock", "paper"))
+  end
+
+  def test_paper_beats_rock_2
+    assert_equal("paper wins", Game.winner("paper", "rock"))
+  end
+
+  def test_scissors_beats_paper_1
+    assert_equal("scissors wins", Game.winner("scissors", "paper"))
+  end
+
+  def test_scissors_beats_paper_2
+    assert_equal("scissors wins", Game.winner("paper", "scissors"))
+  end
+
+end
