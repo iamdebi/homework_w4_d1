d4ba34162059843ec2e35ef08553d3d99cc91580
Debbie <debiurquhart@live.co.uk>
2019-11-25 15:54:06 +0000

    adds server functionality

--- rock_paper_scissors/controller.rb
+++ rock_paper_scissors/controller.rb
@@ -3,3 +3,10 @@ require('sinatra/contrib/all') if development?
 
 require_relative('./models/game')
 also_reload('./models')
+
+get '/game/:item1/:item2' do
+  item1 = params['item1']
+  item2 = params['item2']
+  @results = Game.winner(item1, item2)
+  erb(:results)
+end
